a36aed6b7648cbd91bc3a03ea26e65fcf12c181b
Stephen Juan <55402829+Darkangel12S@users.noreply.github.com>
2021-09-25 20:52:36 -0400

    new changes

---
+++ Proj1/src/RunProject1.java
@@ -0,0 +1,19 @@
+/**
+This method creates an instance of Collection Manager and utilizes the run() method in it to run the program.
+This method should be used to run and test the entire program. 
+@author Stephen Juan, David Halim
+*/
+
+public class RunProject1
+{ 
+	/**
+	This is a main method that calls Collection manager.
+	@param args - user input -> this is not used
+	*/
+	public static void main(String[] args)
+	{
+		new CollectionManager().run();
+	}
+}
+
+
